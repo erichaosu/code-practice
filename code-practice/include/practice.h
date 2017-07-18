1275cdcb7eef5dae14a954819a2d5e700e3ece14
eric Hao <erichaosu@hotmail.com>
2017-07-18 08:36:32 -0700

    add twosum

--- code-practice/include/practice.h
+++ code-practice/include/practice.h
@@ -1,5 +1,9 @@
 #ifndef PRACTICE_H
 #define PRACTICE_H
+class solution{
 
-
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target);
+};
+typedef solution* solutionPtr;
 #endif
---
+++ code-practice/src/practice.cpp
@@ -0,0 +1,43 @@
+//============================================================================
+// Name        : praactice.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <list>
+#include "../include/practice.h"
+
+using namespace std;
+
+vector<int> solution::twoSum(vector<int>& nums, int target) {
+    int i;
+    vector<int> v(2);
+    std::unordered_map<int,int> *lookupMap = new std::unordered_map<int,int>();
+    for (i =0; i< nums.size(); i++){    
+        int compliment = target - nums[i];
+        auto it = lookupMap->find(compliment);
+        if (compliment > 0 && it != lookupMap->end() ){
+            v = {nums[i], compliment};
+            return v;
+        }else {
+            lookupMap->insert({nums[i],i});
+        }
+    }
+    v = {0,0}; 
+    return  v;      
+}
+
+int main() {
+    vector<int> vec;
+    vector<int> testVector;
+    solutionPtr sol;
+    testVector ={8,7,6,5,4,3,2,1};
+    vec = sol->twoSum(testVector, 9);
+    cout<<"sum of {" <<vec[0]<<","<<vec[1]<<"}"<<endl; 
+}
