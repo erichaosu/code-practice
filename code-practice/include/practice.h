49f157732eded0449e61aaa033937e02f05d1014
eric Hao <erichaosu@hotmail.com>
2017-08-03 16:17:58 -0700

    Add more testcases total 11

--- code-practice/include/practice.h
+++ code-practice/include/practice.h
@@ -1,9 +1,11 @@
 #ifndef PRACTICE_H
 #define PRACTICE_H
+
 class solution{
 
 public:
     std::vector<int> twoSum(std::vector<int>& nums, int target);
+    bool isUnique(const std::string &str);
 };
 typedef solution* solutionPtr;
 #endif
--- code-practice/src/practice.cpp
+++ code-practice/src/practice.cpp
@@ -14,23 +14,101 @@
 #include "../include/practice.h"
 
 using namespace std;
-
+// find any two number sum which equal to target
 vector<int> solution::twoSum(vector<int>& nums, int target) {
     int i;
     vector<int> v(2);
     std::unordered_map<int,int> *lookupMap = new std::unordered_map<int,int>();
-    for (i =0; i< nums.size(); i++){    
+    //for each number 
+    for (i =0; i< nums.size(); i++){
+        //find its compliment     
         int compliment = target - nums[i];
+        //search map of this compliment
         auto it = lookupMap->find(compliment);
+        // if complement >0 and found num in map
         if (compliment > 0 && it != lookupMap->end() ){
+            //create vector with two member, number and it compliment
             v = {nums[i], compliment};
             return v;
         }else {
+            //
             lookupMap->insert({nums[i],i});
         }
     }
     v = {0,0}; 
     return  v;      
+}
+//check if all chaaracter in string is unique
+bool isUnique(const string &str)
+{
+    //
+    int str_size = str.size();
+    for (int i = 0; i <str_size; i++){
+        for (int j=i+1; j < str_size; j ++){
+            if (str[j] == *(str.end())) break;
+            if (str[i] == str[j]) return false;
+        }
+    }
+    return true;
+}
+
+int extractUniqueChar(string s){
+    // get the string length
+    int str_len = s.size();
+    cout<<"len "<<str_len<<endl;;
+    string::iterator it = s.begin();
+
+    // new char vector, and int vector
+    vector<char> ch(str_len);
+    vector<int>  num(str_len, 0);
+    int i,j,k;
+    bool dup;
+    // start from first char, save into char vector, +1 in int vect
+    for( i =0; i < str_len && it!= s.end() ; i++){
+        ch[i] = *it;
+        num[i] ++;
+        // start from next char, until end str, if there is duplicate
+        // num[i]++ find number of duplicate
+        // this is to forward check if there is dup 
+        for (j = distance(s.begin(), it)+1; j < str_len; j++){
+            if (ch[i] == s.at(j)){
+                num[i] ++;
+            }
+        }
+        it ++;
+        // check backward if there is dup
+        // if there is dup, skip it
+        while (it != s.end()){
+            for(k =0; k<=i; k++){
+                if (ch[k]==*it) {
+                    dup = true;
+                }
+            }
+            if (dup) {
+            	it++;
+            	dup = false;
+            }else {
+            	break;
+            }
+        }
+    }
+    // char[] vect will have only unique char
+    // num[]has number of dup for that char
+    for (i=0; i<str_len;i++){
+        if(num[i] !=0){
+            for (j =0; j<num[i]; j++){
+                cout<<ch[i];
+            }
+            cout<<" "<<num[i];
+            cout<<endl;
+        }
+    }
+    //form a str from char vector
+    std::string str(ch.begin(), ch.end());
+    cout<< "ch str "<<str<<endl;
+
+    return 0;
+    
 }
 
 int main() {
@@ -41,6 +119,9 @@ int main() {
     testVector ={8,7,6,5,4,3,2,1};
     vec = sol.twoSum(testVector, 9);
     cout<<"sum of {" <<vec[0]<<","<<vec[1]<<"}"<<endl;
-    cout << "hello Bryan!!"<<endl;
+
+    //check if string is unique
+    string str = "banana";
+    cout<< str<<"is "<<(isUnique(str)?"unique":"not unique")<<endl;
      
 }
